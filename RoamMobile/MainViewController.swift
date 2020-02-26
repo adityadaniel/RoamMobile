591eb7e7b2c90b6f3f988ab9db567a9e70e55642
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2020-02-26 12:09:25 +0700

    initial commit

---
+++ RoamMobile/MainViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  RoamMobile
+//
+//  Created by Daniel Aditya Istyana on 26/02/20.
+//  Copyright © 2020 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class MainViewController: UIViewController {
+  
+  let config = WKWebViewConfiguration()
+  let wkController = WKUserContentController()
+  
+  let disableZoomScript = "var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1'); document.getElementsByTagName('head')[0].appendChild(meta);"
+  
+  var webView: WKWebView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .white
+    
+    let disableZoom = WKUserScript(source: disableZoomScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+    wkController.addUserScript(disableZoom)
+    
+    // needed in order to be able to sign in when using Google service
+    config.applicationNameForUserAgent = "Version/8.0.2 Safari/600.2.5"
+    config.userContentController = wkController
+    
+    webView = WKWebView(frame: .zero, configuration: config)
+    let url = URL(string: "https://roamresearch.com")
+    let urlRequest = URLRequest(url: url!)
+    webView.load(urlRequest)
+    
+    webView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(webView)
+    
+    NSLayoutConstraint.activate([
+      webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+    ])
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .darkContent
+  }
+  
+  
+}
+
