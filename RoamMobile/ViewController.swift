4fa80d6c01cd26519f24fa6c01b43d284f781048
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2020-02-26 09:32:19 +0700

    Initial Commit

---
+++ RoamMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoamMobile
+//
+//  Created by Daniel Aditya Istyana on 26/02/20.
+//  Copyright © 2020 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
